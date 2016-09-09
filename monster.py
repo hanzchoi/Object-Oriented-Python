91937073ed1da6d3b40f86495b171967a3223693
Hans Choi <personabe1984@gmail.com>
2016-09-09 17:16:16 -0400

    testing commit

--- monster.py
+++ monster.py
@@ -1,8 +1,18 @@
 # from monster import Monster
+import random
+
+COLOR = ['yellow', 'red', 'blue', 'green']
 
 
 class Monster:
+    min_hit_points = 1
+    max_hit_points = 1
+    min_experience = 1
+    max_experience = 1
     hit_points = 1
+
+
+
     color = 'yellow'
     weapon = 'sword'
     sound = 'roar'
